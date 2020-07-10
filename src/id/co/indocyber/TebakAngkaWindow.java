0494bffe7059a7eb2910fcd089b8834396d36543
Zulfahmi Andri <zulfahmi.andri@gmail.com>
2020-07-10 18:52:55 +0700

    Update TebakAngkaWindow.java
    update to PR

--- src/id/co/indocyber/TebakAngkaWindow.java
+++ src/id/co/indocyber/TebakAngkaWindow.java
@@ -1,5 +1,5 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
+ * License by Zulfahmi (C) 2020
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
